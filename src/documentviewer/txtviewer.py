019d513d88c475c622d2a1ca2520558d2ecaf3af
ghrimx <debruycker.v@gmail.com>
2025-09-09 10:26:03 +0200

    added zoom-in & zoom-out

--- src/documentviewer/txtviewer.py
+++ src/documentviewer/txtviewer.py
@@ -3,15 +3,16 @@ import logging
 from qtpy import (QtWidgets, QtCore, QtGui, Slot)
 
 from documentviewer.viewerwidget import ViewerWidget
-
-from database.dbstructure import Document
+from qt_theme_manager import theme_icon_manager
 
 logger = logging.getLogger(__name__)
+
 
 class TxtViewer(ViewerWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._document = None
+        self.zoom_factor: int = 0
 
     @classmethod
     def viewerName(cls):
@@ -22,12 +23,16 @@ class TxtViewer(ViewerWidget):
         return [".txt", ".md"]
     
     def initViewer(self):
-        self._textreader = QtWidgets.QPlainTextEdit(self)
-        self._textreader.setReadOnly(True)
+        self.left_pane.hide()
+        self._toolbar.removeAction(self.fold_left_pane)
+        self._toolbar.removeAction(self.action_first_separator)
+
+        self.viewer = QtWidgets.QPlainTextEdit(self)
+        self.viewer.setReadOnly(True)
 
         self.scroll_area.setBackgroundRole(QtGui.QPalette.ColorRole.Light)
         self.scroll_area.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.scroll_area.setWidget(self._textreader)
+        self.scroll_area.setWidget(self.viewer)
 
         # Citation
         self.action_cite.triggered.connect(lambda: self.cite(self.citation()))
@@ -39,6 +44,39 @@ class TxtViewer(ViewerWidget):
 
         # Create Signage
         self._toolbar.insertAction(self._toolbar_spacer, self.action_create_child_signage)
+
+        # Zoom
+        zoom_in = QtGui.QAction(theme_icon_manager.get_icon(":zoom-in"), "Zoom In", self)
+        zoom_in.setShortcut(QtGui.QKeySequence.StandardKey.ZoomIn)
+        zoom_in.triggered.connect(lambda: self.zoom(1))
+        self._toolbar.insertAction(self.toolbarFreeSpace(), zoom_in)
+
+        zoom_out = QtGui.QAction(theme_icon_manager.get_icon(":zoom-out"), "Zoom Out", self)
+        zoom_out.setShortcut(QtGui.QKeySequence.StandardKey.ZoomOut)
+        zoom_out.triggered.connect(lambda: self.zoom(-1))
+        self._toolbar.insertAction(self.toolbarFreeSpace(), zoom_out)
+
+        reset_zoom = QtGui.QAction(theme_icon_manager.get_icon(":find-replace-line"), "Reset Zoom", self)
+        reset_zoom.setShortcut("Ctrl+0")
+        reset_zoom.triggered.connect(self.resetZoom)
+        self._toolbar.insertAction(self.toolbarFreeSpace(), reset_zoom)
+
+    def zoom(self, f: int):
+        if f > 0:
+            self.viewer.zoomIn(1)
+        elif f < 0:
+            self.viewer.zoomOut(1)
+        
+        self.zoom_factor += f
+
+    def resetZoom(self):
+        while self.zoom_factor != 0:
+            if self.zoom_factor > 0:
+                self.viewer.zoomOut(1)
+                self.zoom_factor -= 1
+            if self.zoom_factor < 0:
+                self.viewer.zoomIn(1)
+                self.zoom_factor += 1
 
     def loadDocument(self, filepath: str = ""):
         if filepath == "":
@@ -54,10 +92,12 @@ class TxtViewer(ViewerWidget):
             return
 
         text_stream = QtCore.QTextStream(file)
-        self._textreader.setPlainText(text_stream.readAll())
+        self.viewer.setPlainText(text_stream.readAll())
+        self.zoom_factor = 3
+        self.viewer.zoomIn(self.zoom_factor)
 
     def widget(self):
-        return self._textreader
+        return self.viewer
 
     Slot()
     def citation(self) -> str:
@@ -69,5 +109,5 @@ class TxtViewer(ViewerWidget):
     def source(self) -> str:
         title = self._document.title
         viewer = self.viewerName()
-        source = f'{{"application":"InspectorMate", "module":"{viewer}", "item":"document", "item_title":"{title}"}}'
+        source = f'{{"application":"InspectorMate", "module":"{self.viewerName()}", "item":"document", "item_title":"{title}"}}'
         return source
